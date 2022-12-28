828779a75d511695486aaf719de96dfc96c41fcf
Roman Tinkov <drakroman@gmail.com>
2022-12-28 17:54:08 +0100

    Go

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
 
-@app.route("/test")
-def test():
-    return "Hello, test Flask!"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+if __name__ == '__main__': app.run(debug=True)
